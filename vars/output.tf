fead55c08b84dc648ab395b752c0fc02929ac787
tejudevops <saitejaswini8121@gmail.com>
2025-04-08 22:18:19 +0530

    terraform

---
+++ vars/output.tf
@@ -0,0 +1,11 @@
+output "v1" {
+    value = var.v1
+    }
+
+output "v2" {
+    value = var.v2[0]
+    }
+
+output "v3" {
+    value = var.v3["abc"]
+    }
---
+++ vars/variables.tf
@@ -0,0 +1,14 @@
+variable "v1" {
+    default = "Hello World"
+    }
+
+variable "v2" {
+    default = [ "Hi",123,true ]
+    }
+
+variable "v3" {
+    default= {
+        abc = 100
+        xyz = "five hundred"
+        }
+    }
