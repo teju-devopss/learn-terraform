6078c247700299b468509d89b819866d95b7c9b1
tejudevops <saitejaswini8121@gmail.com>
2025-04-08 23:27:45 +0530

    terraform

---
+++ expense-v1/main.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-05c761dbd36f70805"]
+
+  tags = {
+    Name = "frontend-dev"
+  }
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-05c761dbd36f70805"]
+
+  tags = {
+    Name = "backend-dev"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-05c761dbd36f70805"]
+
+  tags = {
+    Name = "mysql-dev"
+  }
+}
