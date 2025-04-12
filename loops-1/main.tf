7dc2f08d898b104e4836448a2a3a0db21034eec8
tejudevops <saitejaswini8121@gmail.com>
2025-04-12 15:49:50 +0530

    terraform

---
+++ loops-1/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "test" {
+  count                  = 3
+  ami                    = "ami-0b4f379183e5706b9"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-05c761dbd36f70805"]
+
+  tags = {
+    Name = "test"
+  }
+}
