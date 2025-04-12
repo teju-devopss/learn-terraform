9ef0a11f690038b8b3b5c0dd5ca209204400711a
tejudevops <saitejaswini8121@gmail.com>
2025-04-12 17:14:17 +0530

    terraform

--- loops-2/main.tf
+++ loops-2/main.tf
@@ -29,6 +29,6 @@ resource "null_resource" "test1" {
 resource "null_resource" "test2" {
   for_each = var.fruits2
   provisioner "local-exec" {
-    command = "echo ${lookup(each.value,"name","null" )} = ${lookup(each.value,quantity,"null" )}"
+    command = "echo ${lookup(each.value,"name","null" )} = ${lookup(each.value,"quantity","null" )}"
   }
 }
