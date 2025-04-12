9a758832131b2bea314329846fea97ae816e3e33
tejudevops <saitejaswini8121@gmail.com>
2025-04-12 16:03:12 +0530

    terraform

--- loops-1/main.tf
+++ loops-1/main.tf
@@ -1,19 +1,24 @@
-# resource "aws_instance" "test" {
-#   count                  = 3
-#   ami                    = "ami-0b4f379183e5706b9"
-#   instance_type          = "t2.micro"
-#   vpc_security_group_ids = ["sg-05c761dbd36f70805"]
-#
-#   tags = {
-#     Name = "test-${count.index}"
-#   }
+variable "components" {
+  default = ["frontend","backend","mysql"]
+}
+
+resource "aws_instance" "components" {
+  count                  = length(var.components)
+  ami                    = "ami-0b4f379183e5706b9"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-05c761dbd36f70805"]
+
+  tags = {
+    Name = element(var.components,count.index )
+  }
+}
+
+
+# resource "null_resource" "demo" {
+#   count = 10
 # }
-
-resource "null_resource" "demo" {
-  count = 10
-}
-
-output "demo" {
-  value = null_resource.demo.*.id
-}
+#
+# output "demo" {
+#   value = null_resource.demo.*.id
+# }
 # as we used count here we will use * in the output to access it
