776b06618adc14cdb782f8c8cf74664d5be3fe4d
tejudevops <saitejaswini8121@gmail.com>
2025-04-13 14:59:02 +0530

    terraform

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -4,11 +4,5 @@ terraform {
     key    = "test/terraform.tfstate"
     region = "us-east-1"
   }
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.3"
-    }
-  }
 }
 resource "null_resource" "test" {}
